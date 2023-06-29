1cb0b6953c8f9becc51c93952fbfbc9f20aadb85
EdwardC20 <eduardo.contreras1902@alumnos.ubiobio.cl>
2023-06-29 02:30:40 -0400

    login

--- resermedFront/src/app/app.module.ts
+++ resermedFront/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './components/home/home.component';
     HeaderComponent,
     FooterComponent,
     HomeComponent,
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
