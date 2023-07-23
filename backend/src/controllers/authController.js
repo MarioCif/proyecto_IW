427ec660335a11c047ee43eccfb4add903c59ef5
Joaquin Canales <joaquin.vcv@gmail.com>
2023-07-22 23:56:28 -0400

    login con jwt en backend

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,3 +1,4 @@
+import jwt from 'jsonwebtoken';
 import { compareP } from "../helpers/handleBcrypt.js";
 import { Usuario } from "../models/Usuario.js";
 import { Medico } from "../models/Medico.js";
@@ -6,53 +7,62 @@ import { Mantenedor } from "../models/Mantenedor.js";
 export const login = async (req, res) => {
 
     const { email, password } = req.body;
-    var sessionToken = '';
-    
-    var user = await Usuario.findOne({
-        where: { email }
-    })
-    console.log(user)
-    if (!user) {
+    var userType = '';
 
-        user = await Medico.findOne({
+    try {
+
+        const user = await Usuario.findOne({
             where: { email }
         })
-
-        if(!user){
-
-            user = await Mantenedor.findOne({
+        
+        if (!user) {
+    
+            user = await Medico.findOne({
                 where: { email }
             })
-
-            if(!user) {
-                res.status(404);
-                res.send({ error: 'Usuario no registrado' });
-                return
+    
+            if(!user){
+    
+                user = await Mantenedor.findOne({
+                    where: { email }
+                })
+    
+                if(!user) {
+                    res.status(404);
+                    res.send({ error: 'Usuario no registrado' });
+                    return;
+                }else{
+                    userType = 'mantenedor'
+                }
             }else{
-                sessionToken = 'mantenedor'
+                userType = 'medico'
             }
         }else{
-            sessionToken = 'medico'
+            userType = 'usuario';
         }
-    }else{
-        sessionToken = 'usuario';
-    }
-
     
+        const checkPassword = await compareP(password, user.password);
 
-    const checkPassword = await compareP(password, user.password);
+        if(!checkPassword){
+            return res.status(400).json({ message: "Correo o contraseña incorrectos" });
+        }
 
-    if (checkPassword) {
-        res.send({
-            data: user,
-            sessionToken: sessionToken
-        });
-        return
-    } else {
-        res.status(404);
-        res.send({
-            error: 'Credenciales incorrectas'
-        })
-        return
+        const payload = {
+            id: user.id,
+            nombre: user.nombre,
+            email: user.email,
+            userType: userType
+        }
+
+        console.log(payload);
+        const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '12h'})
+
+        res.json({ token });
+        
+    } catch (error) {
+        res.status(500).json({ message: "Internal server error" });
+
     }
+    
+    
 }
