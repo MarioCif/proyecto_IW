8424a31d46bc66dd08c83f33557b79861decc8e3
Joaquin Canales <joaquin.vcv@gmail.com>
2023-07-23 03:23:59 -0400

    corregir auth router

--- backend/src/routers/authRouter.js
+++ backend/src/routers/authRouter.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { login } from "../controllers/authController.js";
-import { getUserType } from "../middlewares/authMiddleware.js";
 
 const router = Router();
 
